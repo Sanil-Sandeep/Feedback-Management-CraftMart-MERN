487ca359d9b6b21f7f3a21eaab60330df77e0028
Sanil Sandeep <sanil.sandeep001@gmail.com>
2024-10-13 23:14:47 +0530

    update feedbackModel

--- backend/models/feedbackModel.js
+++ backend/models/feedbackModel.js
@@ -1,6 +1,24 @@
 import mongoose from "mongoose";
 
 const feedbackSchema = mongoose.Schema(
+    {
+        name: {
+            type: String,
+            required: true,
+        },
+        email: {
+            type: String,
+            required: true,
+        },
+        message: {
+            type: String,
+            required: true,
+        },
+        reply: {
+            type: String,
+            
+        }
+    },
     
 );
 
