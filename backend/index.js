a65fcb034a311d3324fb0ec1cf6f862cfad5676a
Sanil Sandeep <sanil.sandeep001@gmail.com>
2024-10-13 23:11:02 +0530

    Start project backend

---
+++ backend/index.js
@@ -0,0 +1,44 @@
+import express, { request, response } from "express";
+import { PORT, mongoDBURL } from "./config.js";
+import mongoose from 'mongoose';
+import { Feedback } from './models/feedbackModel.js';
+import feedbackRoute from './routes/feedbackRoute.js';
+import cors from 'cors';
+
+const app = express();
+
+// Middleware for parsing request body
+app.use(express.json());
+
+//Middleware for handling Cors Policy
+//Option 01: Allow all Origins with Default of cors (*)
+app.use(cors());
+//option 02:Allow custom origins
+/*
+app.use(
+   cors({
+      origin: 'http://localhost:5555',
+      methods: ['GET', 'POST', 'PUT', 'DELETE'],
+      allowedHeaders: ['Content-Type'],
+   })
+);
+*/
+
+app.get('/', (request, response) => {
+    console.log(request)
+    return response.status(234).send('Welcome to mern stack tutorial')
+});
+
+app.use('/feedbacks', feedbackRoute);
+
+mongoose
+   .connect(mongoDBURL)
+   .then(() => {
+     console.log('App connected to database');
+     app.listen(PORT, () => {
+        console.log(`App is listning to port: ${PORT}`)
+    });
+   })
+   .catch((error) => {
+      console.log(error);
+   })
