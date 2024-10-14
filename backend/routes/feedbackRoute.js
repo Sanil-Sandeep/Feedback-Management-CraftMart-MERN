aecae78556d24791a3d43e92037aae5f1be086ff
Sanil Sandeep <sanil.sandeep001@gmail.com>
2024-10-14 19:47:04 +0530

    Route for update feedback

--- backend/routes/feedbackRoute.js
+++ backend/routes/feedbackRoute.js
@@ -55,6 +55,31 @@ router.get("/:id", async (request, response) => {
   }
 });
 
+//route for update feedback
+router.put("/:id", async (request, response) => {
+  try {
+    if (!request.body.name || !request.body.email || !request.body.message) {
+      return response.status(400).send({
+        message: "Send all required fields: name, email, message",
+      });
+    }
+
+    const { id } = request.params;
+
+    const result = await Feedback.findByIdAndUpdate(id, request.body);
+
+    if (!result) {
+      return response.status(404).json({ message: "Feedback not found" });
+    }
+    return response
+      .status(200)
+      .send({ message: "Feedback updated succesfully" });
+  } catch (error) {
+    console.log(error.message);
+    response.status(500).send({ message: error.message });
+  }
+});
+
 
 
 export default router;
