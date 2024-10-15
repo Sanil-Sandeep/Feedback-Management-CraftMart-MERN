d1e45e6da1fca425567982fa19a4c541ce7548be
Sanil Sandeep <sanil.sandeep001@gmail.com>
2024-10-15 22:14:06 +0530

    Start frontend

---
+++ frontend/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import {Routes, Route} from 'react-router-dom'
+
+const App = () => {
+  return (
+    <Routes>
+      
+    </Routes>
+  )
+}
+
+export default App
---
+++ frontend/tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        customRed: '#330D0F', // Add your custom color here
+      },
+    },
+  },
+  plugins: [],
+}
+
