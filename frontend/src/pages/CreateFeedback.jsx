ebc989e71ae00268147f944e91baaf0db2d03192
Sanil Sandeep <sanil.sandeep001@gmail.com>
2024-10-15 22:20:50 +0530

    Create CreateFeedback.jsx

---
+++ frontend/src/pages/CreateFeedback.jsx
@@ -0,0 +1,11 @@
+import React, { useState } from 'react';
+import BackButton from '../components/BackButton';
+import Spinner from '../components/Spinner';
+import axios from 'axios';
+import { useNavigate } from 'react-router-dom';
+import { useSnackbar } from 'notistack';
+
+
+
+
+export default CreateFeedback;
