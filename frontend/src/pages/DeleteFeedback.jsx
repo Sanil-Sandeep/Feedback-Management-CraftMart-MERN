fe2e1f2d3a4ee5c06c0a1bf2a7524d66bb0e86ae
Sanil Sandeep <sanil.sandeep001@gmail.com>
2024-10-16 23:03:43 +0530

    Add handleDelete function

--- frontend/src/pages/DeleteFeedback.jsx
+++ frontend/src/pages/DeleteFeedback.jsx
@@ -11,6 +11,23 @@ const DeleteFeedback = () => {
   const { id } = useParams();
   const { enqueueSnackbar } = useSnackbar();
 
+  const handleDeleteFeedback = () => {
+    setLoading(true);
+    axios
+      .delete(`http://localhost:5555/feedbacks/${id}`)
+      .then(() => {
+        setLoading(false);
+        enqueueSnackbar('Feedback Deleted Successfully', {variant: 'success'});
+        navigate('/feedbacks/full');
+      })
+      .catch((error) => {
+        setLoading(false);
+        //alert('An error happened. Please check the console.');
+        enqueueSnackbar('Error', {variant: 'error'});
+        console.log(error);
+      });
+  };
+
   
 
 export default DeleteFeedback;
