1656f8c07f3fe3cb1c2ddb4f3e7361fd8075bd81
Sanil Sandeep <sanil.sandeep001@gmail.com>
2024-10-16 23:03:07 +0530

    Update DeleteFeedback.jsx

--- frontend/src/pages/DeleteFeedback.jsx
+++ frontend/src/pages/DeleteFeedback.jsx
@@ -6,6 +6,11 @@ import { useNavigate, useParams } from 'react-router-dom';
 import { useSnackbar } from 'notistack';
 
 const DeleteFeedback = () => {
- 
+  const [loading, setLoading] = useState(false);
+  const navigate = useNavigate();
+  const { id } = useParams();
+  const { enqueueSnackbar } = useSnackbar();
+
+  
 
 export default DeleteFeedback;
