327b07274031a006b9ca623817dd7d3a19a53da6
Sanil Sandeep <sanil.sandeep001@gmail.com>
2024-10-16 23:02:22 +0530

    Create DeleteFeedback.jsx

---
+++ frontend/src/pages/DeleteFeedback.jsx
@@ -0,0 +1,11 @@
+import React, { useState } from 'react';
+import BackButton from '../components/BackButton';
+import Spinner from '../components/Spinner';
+import axios from 'axios';
+import { useNavigate, useParams } from 'react-router-dom';
+import { useSnackbar } from 'notistack';
+
+const DeleteFeedback = () => {
+ 
+
+export default DeleteFeedback;
