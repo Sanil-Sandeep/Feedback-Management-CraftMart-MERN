36b5c5caa6fc44e82e39baf6d3d5f1e173338a57
Sanil Sandeep <sanil.sandeep001@gmail.com>
2024-10-18 22:11:35 +0530

    Create FeedbackDashbord.jsx

---
+++ frontend/src/pages/FeedbackDashbord.jsx
@@ -0,0 +1,14 @@
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import Spinner from '../components/Spinner';
+import { Link } from 'react-router-dom';
+import { AiOutlineEdit } from 'react-icons/ai';
+import { BsInfoCircle } from 'react-icons/bs';
+import { MdOutlineAddBox, MdOutlineDelete } from 'react-icons/md';
+import jsPDF from 'jspdf';
+import 'jspdf-autotable';
+import logo from '../images/logo.png'
+
+
+
+export default FeedbackDashboard;
