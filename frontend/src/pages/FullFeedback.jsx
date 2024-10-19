026655227162ea708e20283d46c04c49f00ab714
Sanil Sandeep <sanil.sandeep001@gmail.com>
2024-10-19 21:07:08 +0530

    Update FullFeedback.jsx

--- frontend/src/pages/FullFeedback.jsx
+++ frontend/src/pages/FullFeedback.jsx
@@ -21,7 +21,51 @@ const FullFeedback = () => {
       });
   }, []);
 
-  
+  return (
+    <div style={styles.page}>
+      <div style={styles.container}>
+        <div style={styles.header}>
+          <h1 style={styles.title}>Tell us what you think!</h1>
+          <Link to='/feedbacks/create'>
+            <button style={styles.button}>Give Feedback</button>
+          </Link>
+        </div>
+
+        <br></br><br></br>
+
+        {loading ? (
+          <Spinner />
+        ) : (
+          <div style={styles.feedbackSection}>
+            {feedbacks.map((feedback) => (
+              <div key={feedback._id} style={styles.feedbackWrapper}>
+                <div style={styles.feedbackBox}>
+                  <div style={styles.feedbackText}>{feedback.name}</div>
+                  <div style={styles.feedbackMessage}>{feedback.message}</div>
+                </div>
+                <div style={styles.actions}>
+                  <Link to={`/feedbacks/edit/${feedback._id}`} style={styles.link}>
+                    Edit
+                  </Link>
+                  <Link to={`/feedbacks/delete/${feedback._id}`} style={styles.link}>
+                    Delete
+                  </Link>
+                </div>
+                {feedback.reply && (
+                  <div style={styles.replyBox}>
+                    <div style={styles.replyText}>Reply:</div>
+                    <div style={styles.replyMessage}>{feedback.reply}</div>
+                  </div>
+                )}
+              </div>
+            ))}
+          </div>
+        )}
+      </div>
+    </div>
+  );
+};
+
 
 
 export default FullFeedback;
