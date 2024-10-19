8413c022e87c6034caa1fafbb96edb10e08a2b94
Sanil Sandeep <sanil.sandeep001@gmail.com>
2024-10-19 21:05:47 +0530

    Create FullFeedback.jsx

---
+++ frontend/src/pages/FullFeedback.jsx
@@ -0,0 +1,10 @@
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import Spinner from '../components/Spinner';
+import { Link } from 'react-router-dom';
+
+const FullFeedback = () => {
+  
+
+
+export default FullFeedback;
