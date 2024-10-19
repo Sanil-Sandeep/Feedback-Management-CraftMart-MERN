a19a40447dc78131213c32f99574c3a16d528ce8
Sanil Sandeep <sanil.sandeep001@gmail.com>
2024-10-19 21:06:31 +0530

    Update FullFeedback.jsx

--- frontend/src/pages/FullFeedback.jsx
+++ frontend/src/pages/FullFeedback.jsx
@@ -4,6 +4,23 @@ import Spinner from '../components/Spinner';
 import { Link } from 'react-router-dom';
 
 const FullFeedback = () => {
+  const [feedbacks, setFeedbacks] = useState([]);
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    setLoading(true);
+    axios
+      .get('http://localhost:5555/feedbacks')
+      .then((response) => {
+        setFeedbacks(response.data.data);
+        setLoading(false);
+      })
+      .catch((error) => {
+        console.log(error);
+        setLoading(false);
+      });
+  }, []);
+
   
 
 
