78130fd54f7cbf82bdcc2920fec27b1bd7e41e9a
Sanil Sandeep <sanil.sandeep001@gmail.com>
2024-10-20 22:09:09 +0530

    Create ReplyFeedback.jsx

---
+++ frontend/src/pages/ReplyFeedback.jsx
@@ -0,0 +1,11 @@
+import React, { useState, useEffect } from 'react';
+import BackButton from '../components/BackButton';
+import Spinner from '../components/Spinner';
+import axios from 'axios';
+import { useNavigate, useParams } from 'react-router-dom';
+import { useSnackbar } from 'notistack';
+
+const ReplyFeedback = () => {
+ 
+
+export default ReplyFeedback;
